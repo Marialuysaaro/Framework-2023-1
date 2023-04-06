cb00b441a9cc46ae1a58b3a532d320475cd96ba1
Marialuysaaro <126828129+Marialuysaaro@users.noreply.github.com>
2023-04-06 17:47:27 -0400

    Add files via upload

--- my-app/src/app/components/login/login.component.ts
+++ my-app/src/app/components/login/login.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  isError = false;
+  isCorrect= false;
 
+  user = ""
+  pwd = ""
+
+  login(){
+    if(this.user == "maria@gmail" && this.pwd == "maria"){
+      this.isCorrect = true;
+      this.isError = false;
+    }
+    else{
+      this.isError = true;
+      this.isCorrect = false;
+    }
+
+    console.log("User = " + this.user + "Password = " + this.pwd)
+  }
 }
