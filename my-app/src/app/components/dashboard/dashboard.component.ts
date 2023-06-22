70ebc1cda84fe3a10b5f0905f067a03617c9738e
Marialuysaaro <126828129+Marialuysaaro@users.noreply.github.com>
2023-06-22 18:25:36 -0400

    22-06

--- my-app/src/app/components/dashboard/dashboard.component.ts
+++ my-app/src/app/components/dashboard/dashboard.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Task } from 'src/app/model/task';
+import { TaskService } from '../task.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent {
-user = "Carolina";
+export class DashboardComponent implements OnInit {
+  ngOnInit(): void {
+  this.loadTasks();
+  console.log(this.tasks)
+  }
+  loadTasks() {
+    console.log('Carregar as taks');
+    this.tasks = this.taskService.tasks;
+  }
 
+
+user = "Maria Luysa";
 data = new Date();
+
+constructor(private router: Router, private taskService: TaskService){}
+
+tasks: Task[] = [];
+
+addTask(){
+  this.router.navigateByUrl('/task', {
+    state: this.tasks,
+  });
+}
 }
